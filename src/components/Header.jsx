0507d469cfbf093c6c67e3e528f69f4081f3cdf8
frostyserge <eyemoroz@gmail.com>
2023-08-30 21:27:52 -0400

    header component basic design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,22 @@
 import MenuItem from "./MenuItem";
+import Link from "next/link";
 import { AiFillHome } from "react-icons/ai";
+import { BsInfoCircleFill } from "react-icons/bs";
 
 export default function Header () {
     return (
-        <div>
-            <div>
+        <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
+            <div className="flex">
                 <MenuItem title="HOME" address="/" Icon={AiFillHome}/>
+                <MenuItem title="ABOUT" address="/about" Icon={BsInfoCircleFill} />
             </div>
             <div>
-
+                <Link href="/">
+                    <h2>
+                        <span className="font-bold bg-yellow-500 py-2 rounded-lg mr-1">IMDb</span>
+                        <span className="text-xl hidden sm:inline">Clone</span>
+                    </h2>
+                </Link>
             </div>
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}"
+    "./src/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
