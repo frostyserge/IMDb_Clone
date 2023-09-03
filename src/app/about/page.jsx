890b7d3a5236308e488c2e19dfde30ed4039f80e
frostyserge <eyemoroz@gmail.com>
2023-09-02 22:17:49 -0400

    created Navbar and a group of two navbar items - Trending and Top rated

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,7 +1,7 @@
 
 export default function About () {
     return (
-        <div className="mx-auto">
+        <div className="mx-auto max-w-6x1 space-y-4 p-4">
             <h1 className="text-2xl font-medium">About Page</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Neque vitae tempus quam pellentesque nec nam aliquam. Nisl vel pretium lectus quam id. Nibh nisl condimentum id venenatis. Sit amet consectetur adipiscing elit pellentesque habitant. Aliquet porttitor lacus luctus accumsan tortor posuere ac ut. Non blandit massa enim nec dui. Auctor urna nunc id cursus metus aliquam eleifend mi. Viverra tellus in hac habitasse platea. At varius vel pharetra vel. Eu consequat ac felis donec. Vel pharetra vel turpis nunc eget lorem dolor sed viverra. Malesuada bibendum arcu vitae elementum. Mi quis hendrerit dolor magna eget. Facilisi etiam dignissim diam quis enim lobortis. Dolor sit amet consectetur adipiscing elit. Pretium aenean pharetra magna ac placerat. Risus in hendrerit gravida rutrum quisque non tellus. Quis lectus nulla at volutpat diam ut venenatis. Eget mauris pharetra et ultrices neque.</p>
         </div>
