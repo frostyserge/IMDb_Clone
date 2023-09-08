086f30e29a0b391ba8aacaef3f77ab72819c92e3
frostyserge <eyemoroz@gmail.com>
2023-09-08 14:52:27 -0400

    added API key from themoviedb.org website and connected to it

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,11 @@
-import Image from 'next/image'
 
-export default function Home() {
+const API_KEY = process.env.API_KEY
+
+export default async function Home({ searchParams }) {
+
+  const genre = searchParams.genre || "fetchTrending";
+  const response = await fetch(`https://api.themoviedb.org/3/${genre === "fetchToprated" ? "movie/top_rated" : "trending/all/week"}?api_key=${API_KEY}&language=en-US&page=1`)
+
   return (
     <main>
       <h1 className="text-blue-400">
