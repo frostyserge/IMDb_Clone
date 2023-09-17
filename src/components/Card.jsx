c102fa3684442ac397e80cd727f69fe76a7a4283
frostyserge <eyemoroz@gmail.com>
2023-09-16 23:19:26 -0400

    added a card component for each movie info to be displayed on the result's parent component

---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Card({result}) {
+  return (
+    <div className="cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow-200">
+        {result.id}
+    </div>
+  )
+}
