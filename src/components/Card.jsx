79b5af36631fd98e3ad546299e8b6d2ec9346910
frostyserge <eyemoroz@gmail.com>
2023-09-16 23:42:13 -0400

    styled Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,13 @@
+import Image from "next/image";
+import Link from "next/link";
 
 
 export default function Card({result}) {
   return (
-    <div className="cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow-200">
-        {result.id}
+    <div className="cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow-200 group">
+        <Link href={`/movie/${result.id}`}>
+            <Image src={`https://image.tmdb.org/t/p/original/${result.backdrop_path || result.poster_path}`} width={500} height={300} className="sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-100" placeholder="blur" blurDataURL="/spinner.svg" alt="image poster"></Image>
+        </Link>
     </div>
   )
 }
