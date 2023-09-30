76fc8d4cb576316f2f45c26aa775e8915892ca62
frostyserge <eyemoroz@gmail.com>
2023-09-29 23:59:00 -0400

    added more info to the movie page

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { PiThumbsUpLight } from 'react-icons/pi';
 
 async function getMovie(movieId) {
     const response = await fetch(
@@ -24,6 +25,25 @@ export default async function MoviePage({ params }) {
                     blurDataURL="/spinner.svg"
                     alt="movie poster"
                 ></Image>
+            </div>
+            <div className="p-2">
+                <h2 className="text-lg mb-3 font-bold">{movie.title || movie.name}</h2>
+                <p className="text-lg mb-3">
+                    <span className="font-semibold mr-1">Overview:</span>
+                    {movie.overview}
+                </p>
+                <p className="mb-3">
+                    <span className="font-semibold mr-1">
+                        Date Released:
+                    </span>
+                    {movie.release_date || movie.first_air_date}
+                </p>
+                <p className="mb-3">
+                    <span className="font-semibold mr-1">
+                        Rating: 
+                    </span>
+                   {movie.vote_count}
+                </p>
             </div>
         </div>
     );
