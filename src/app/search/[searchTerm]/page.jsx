b9235dc217051a9810977191d74dcb1b2531f5f1
frostyserge <eyemoroz@gmail.com>
2023-10-01 00:56:44 -0400

    completeed the search results page

--- src/app/search/[searchTerm]/page.jsx
+++ src/app/search/[searchTerm]/page.jsx
@@ -1,6 +1,7 @@
+import Results from "@/components/Results";
 
 
-export default async function searchPage() {
+export default async function searchPage({ params }) {
 
     const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${params.searchTerm}&language=en-US`);
 
@@ -8,7 +9,17 @@ export default async function searchPage() {
         throw new Error("Error fetching data");
     }
 
+    const data = await response.json();
+
+    const results = data.results;
+
   return (
-    <div>Search Page</div>
+    <div>
+        {results && results.length === 0 && (
+            <h1 className="text-center pt-6">Nothing found by this criteria</h1>
+        )}
+
+        {results && <Results results={results} />}
+    </div>
   )
 }
